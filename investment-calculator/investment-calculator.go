3409d9df990740a1fca0838afa88bc78f2799a52
Viacheslav Filipenko <filipenko.slavik@gmail.com>
2025-01-26 19:02:20 +0200

    feat(investment-calculator): formula calculation

--- investment-calculator/investment-calculator.go
+++ investment-calculator/investment-calculator.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math"
+)
 
+func main() {
+	var investmentAmount float64 = 1000
+	var annualInterestRate float64 = 4.25
+	var numberOfYears int = 10
+
+	var futureInvestmentValue float64 = investmentAmount * math.Pow(1+annualInterestRate/100, float64(numberOfYears))
+
+	fmt.Println("Accumulated value is", futureInvestmentValue)
 }
