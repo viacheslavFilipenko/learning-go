92d8160e34ad60076f6c3bbc67fdb390dae85e0a
Viacheslav Filipenko <filipenko.slavik@gmail.com>
2025-01-26 19:16:11 +0200

    feat(investment-calculator): add inflation support

--- investment-calculator/investment-calculator.go
+++ investment-calculator/investment-calculator.go
@@ -6,11 +6,15 @@ import (
 )
 
 func main() {
-	var investmentAmount float64 = 1000
-	var annualInterestRate float64 = 4.25
-	var numberOfYears int = 10
+	const inflationRate float64 = 3.5
 
-	var futureInvestmentValue float64 = investmentAmount * math.Pow(1+annualInterestRate/100, float64(numberOfYears))
+	investmentAmount := 1000.0
+	years := 5.0
+	annualInterestRate := 4.25
+
+	var futureInvestmentValue float64 = investmentAmount * math.Pow(1+annualInterestRate/100, years)
+	var realFutureInvestmentValue float64 = futureInvestmentValue / math.Pow(1+inflationRate/100, years)
 
 	fmt.Println("Accumulated value is", futureInvestmentValue)
+	fmt.Println("Real accumulated value is", realFutureInvestmentValue)
 }
