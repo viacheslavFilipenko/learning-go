d28e851363cce8bf0acd485a8a3602ca04f5f2b2
Viacheslav Filipenko <filipenko.slavik@gmail.com>
2025-01-26 19:42:16 +0200

    feat(investment-calculator): add getUserInput function

--- investment-calculator/investment-calculator.go
+++ investment-calculator/investment-calculator.go
@@ -12,20 +12,22 @@ func main() {
 	var years float64
 	var annualInterestRate float64
 
-	fmt.Print("Enter investment amount: ")
-	fmt.Scanf("%f", &investmentAmount)
-
-	fmt.Print("Enter annual interest rate in percentage: ")
-	fmt.Scanf("%f", &annualInterestRate)
-
-	fmt.Print("Enter number of years: ")
-	fmt.Scanf("%f", &years)
+	investmentAmount = getUserInput("Enter investment amount: ")
+	annualInterestRate = getUserInput("Enter annual interest rate in percentage: ")
+	years = getUserInput("Enter number of years: ")
 
 	var futureInvestmentValue float64 = calculateInvestment(investmentAmount, annualInterestRate, years)
 	var realFutureInvestmentValue float64 = calculateInvestment(futureInvestmentValue, inflationRate, years)
 
 	fmt.Printf("Accumulated value is %.0f\n", futureInvestmentValue)
 	fmt.Printf("Real accumulated value is %.0f\n", realFutureInvestmentValue)
+}
+
+func getUserInput(text string) float64 {
+	var input float64
+	fmt.Print(text)
+	fmt.Scanf("%f", &input)
+	return input
 }
 
 func calculateInvestment(investmentAmount float64, annualInterestRate float64, years float64) float64 {
